cb5c82c9823c26881ec2f70218c0353670573b1a
kimvinh <42789132+kimvinh@users.noreply.github.com>
2020-09-13 21:44:33 -0700

    Fixed bugs

--- LexicalChecking.h
+++ LexicalChecking.h
@@ -12,19 +12,41 @@ string checkChar(string word)
 		return "OPERATOR";
 	}
 	else if (word == "int" || word == "float" || word == "bool" || word == "true" || word == "false" || word == "if" || word == "else" || word == "then" ||
-		word == "endif" || word == "while" || word == "do" || word == "for" || word == "input" || word == "output" || word == "and" || word == "or" || word == "not") {
+		     word == "endif" || word == "while" || word == "do" || word == "for" || word == "input" || word == "output" || word == "and" || word == "or" || 
+			 word == "not" || word == "whileend") {
 		return "KEYWORD";
 	}
 	else if (word == "'" || word == "(" || word == ")" || word == "{" || word == "}" || word == "[" || word == "]" || word == "," ||
-		word == "." || word == ":" || word == ";") {
+		     word == "." || word == ":" || word == ";") {
 		return "SEPARATOR";
 	}
-	else if (word == "0" || word == "1" || word == "2" || word == "3" || word == "4" || word == "5" || word == "6" || word == "7" || word == "8" || word == "9") {
-		return "INTEGER";
-	}
 	else {
-		return "IDENTIFIER";
+		// Check "word" if it is an integer
+		// If yes, return "INTEGER"
+		// If no, continue to check "REAL"
+		for (int i = 0; i < word.length(); i++) {
+			if (isdigit(word[i])) {
+				if (i == word.length() - 1) {
+					return "INTEGER";
+				}
+				continue;
+			}
+			else {
+				break;
+			}
+		}
+
+		// Check "word" if it is a real
+		// If yes, return "REAL"
+		// If no, "word" can only be "IDENTIFIER"
+		for (int i = 0; i < word.length(); i++) {
+			if (word[i] == '.') {
+				return "REAL";
+			}
+		}
 	}
+		
+	return "IDENTIFIER";
 }
 
 LinkedList<string> checkWord(string line, int &blockComment)
@@ -38,18 +60,20 @@ LinkedList<string> checkWord(string line, int &blockComment)
 		testWord = line[c];
 
 		if (testWord == "!")
-			++blockComment;
+			blockComment++;
 
-		if (blockComment % 2 != 0)
-			continue;
-		else if (testWord == " ") {
+		if (blockComment == 1 || blockComment == 2) {
+			if (blockComment == 2) {
+				blockComment = 0;
+			}
+ 			continue;
+		} else if (testWord == " ") {
 			if (!word.empty()) {
 				list.push_back(checkChar(word), word);
 				word.clear();
 			}
 			continue;
-		}
-		else if (checkChar(testWord) == "OPERATOR") {
+		} else if (checkChar(testWord) == "OPERATOR") {
 			if (!word.empty()) {
 				list.push_back(checkChar(word), word);
 				word.clear();
@@ -64,24 +88,32 @@ LinkedList<string> checkWord(string line, int &blockComment)
 				word.clear();
 			}
 			continue;
-		}
-		else if (checkChar(testWord) == "SEPARATOR") {
+		} else if (checkChar(testWord) == "SEPARATOR") {
 			if (!word.empty()) {
-				list.push_back(checkChar(word), word);
-				word.clear();
+				// Check if the next letter after "." is an integer
+				// If yes, then "." is not a separator in this case
+				// Then we know that the word must be a real number
+				// Then continue checking the rest number in the next letters of a real number 
+				if (isdigit(line[c + 1])) {
+					word += line[c];
+					continue;
+				}
+				else {
+					list.push_back(checkChar(word), word);
+					word.clear();
 
-				word.push_back(line.at(c));
-				list.push_back(checkChar(word), word);
-				word.clear();
+					word.push_back(line.at(c));
+					list.push_back(checkChar(word), word);
+					word.clear();
+				}
 			}
 			else {
 				word.push_back(line.at(c));
-				list.push_back(checkChar(word), word);
+				list.push_back("SEPARATOR", word);
 				word.clear();
 			}
 			continue;
-		}
-		else if (c == line.length() - 1) {
+		} else if (c == line.length() - 1) {
 			word.push_back(line.at(c));
 			list.push_back(checkChar(word), word);
 			word.clear();
--- Main.cpp
+++ Main.cpp
@@ -14,12 +14,12 @@ int main() {
 	LinkedList<string> list;
 
 	cout << "\t=================================\n"
-		<< "\t   WELCOME TO LEXICAL ANALYZER\n"
-		<< "\t=================================\n"
-		<< "\t           MAIN MENU\n"
-		<< "   1. Manual\n"
-		<< "   2. Read from a file\n"
-		<< "   3. Exit\n\n";
+		 << "\t   WELCOME TO LEXICAL ANALYZER\n"
+		 << "\t=================================\n"
+		 << "\t           MAIN MENU\n"
+		 << "   1. Manual\n"
+		 << "   2. Read from a file\n"
+		 << "   3. Exit\n\n";
 	choice = getBoundedInt("Select your choice: ", 1, 3);
 
 	switch (choice) 
@@ -39,29 +39,31 @@ int main() {
 				cout << "Input the file's name: ";
 				getline(cin, fileName);
 				fstream myFile(fileName);
-				if (myFile.fail())
+				if (myFile.is_open())
 				{
-					cout << "Could not open \"" << fileName << "\" file or file does not exist.\n";
-					continue;
-				}
-				else {
 					string readFile;
 					while (getline(myFile, readFile)) {
 						list = list + checkWord(readFile, blockComment);
 					}
+				}
+				else {
+					cout << "Could not open \"" << fileName << "\" file or file does not exist.\n";
+					continue;
 				}
 
 				myFile.close();
 				break;
 			}
 		} break;
-		default:	return 0;
+		default:
+			return 0;
 	}
 
 
 	cout << endl;
 	cout << left;
 	cout << setw(20) << "TOKEN" << setw(11) << " " << setw(20) << "LEXEMES" << endl;
+	cout << "--------------------------------------\n";
 	while (!list.isEmpty()) {
 		cout << setw(20) << list.showToken() << setw(11) << "=" << setw(20) << list.showLexeme() << endl;
 		list.pop_front();
